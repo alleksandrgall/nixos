b06f3115e43db0b9504309747e30489d289f3f32
Ivan Iablochkin <alleksandrgall@gmail.com>
2024-07-03 22:10:25 +0000

    fix vscode cringe

--- flake.nix
+++ flake.nix
@@ -2,12 +2,14 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
+    vscode-server.url = "github:nix-community/nixos-vscode-server";
   };
 
   outputs = {
     self,
     nixpkgs,
     nixos-wsl,
+    vscode-server,
     home-manager,
   }: {
     nixosConfigurations.nixos = let
@@ -18,6 +20,7 @@
         system = "x86_64-linux";
         modules = [
           nixos-wsl.nixosModules.default
+          vscode-server.nixosModules.default
           ./packages.nix
           ./git.nix
           ./zsh.nix
@@ -29,6 +32,8 @@
           }: {
             system.stateVersion = "24.05";
             wsl.enable = true;
+            programs.nix-ld.enable = true;
+            services.vscode-server.enable = true;
             nix.extraOptions = "experimental-features = nix-command flakes";
             nix.trustedUsers = ["root" "${localName}"];
             wsl.defaultUser = localName;
@@ -49,21 +54,6 @@
               useUserPackages = true;
               users.${localName}.home.stateVersion = "24.05";
             };
-
-            ### КРИНЖ
-            systemd.user.paths."vscode-remote-workaround" = {
-              wantedBy = ["default.target"];
-              pathConfig.PathChanged = "%h/.vscode-server/bin";
-            };
-            systemd.user.services."vscode-remote-workaround" = {
-              script = ''
-                for i in ~/.vscode-server/bin/*; do
-                  echo "Fixing vscode-server in $i..."
-                  ln -sf ${pkgs.nodejs}/bin/node $i/node
-                done
-              '';
-            };
-            ###
           })
         ];
       };
